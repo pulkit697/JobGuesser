3796928f9e249593303cdeba4a3cc8e449e25d99
pulkit697 <pulkitgarg697@gmail.com>
2025-09-28 01:23:43 +0530

    case insentive enum deserialization

--- src/main/java/dev/pulkit/model/enums/Role.java
+++ src/main/java/dev/pulkit/model/enums/Role.java
@@ -1,5 +1,8 @@
 package dev.pulkit.model.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Role {
     SYSTEM("system"),
     ASSISTANT("assistant"),
@@ -12,7 +15,19 @@ public enum Role {
         this.value = value;
     }
 
+    @JsonValue
     public String getValue() {
         return value;
+    }
+
+    @JsonCreator
+    public Role fromString(String s) {
+        if (s == null)  return null;
+        for (Role role: Role.values()) {
+            if (role.value.equalsIgnoreCase(s)) {
+                return role;
+            }
+        }
+        return ASSISTANT;
     }
 }
